687a562aaf1cd3489390cff172829c7dd8786e49
SrStamm <mirkostamm19@gmail.com>
2025-10-22 10:29:25 +0100

    feat(model): Implemented basic visualization model

--- app/models/card.py
+++ app/models/card.py
@@ -10,3 +10,6 @@ class Card(Base):
     meaning = Column(String, nullable=False)
     image = Column(String, nullable=False)
     energy = Column(Integer, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Card(id={self.id}, name={self.name}, meaning={self.meaning}, image={self.image}, energy={self.energy}>"
